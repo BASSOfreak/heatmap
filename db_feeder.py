91b3b7846b9959c98fbcfc1b1430aadf17184941
BASSOfreak <bassofreak@gmail.com>
2024-09-27 22:09:22 +0200

    db_feeder uses new approach

--- db_feeder.py
+++ db_feeder.py
@@ -6,6 +6,7 @@ from db_connection.db_methods import insert_gps_file
 from gpsfile.BlobHandling import convert_into_binary
 from gpsconverter.HashFile import hash_file
 from gpsfile.GpsFileWithPts import GpsFileWithPts
+import module_variables
 
 def insert_files_into_db():
     input_folder = module_variables.INPUT_FILES_LOCATION
@@ -13,16 +14,8 @@ def insert_files_into_db():
     onlyfiles = [f for f in listdir(input_folder) if isfile(join(input_folder, f))]
     for file_name in onlyfiles:
         print(file_name)
-        convertFile(input_folder, file_name, module_variables.TEMP_OUTPUT_FILES_LOCATION)
-        convertedFileLocation = str(module_variables.TEMP_OUTPUT_FILES_LOCATION + file_name)
-        print(convertedFileLocation)
-        gpsFile = GpsFileWithPts(
-                file_name,
-                convert_into_binary(convertedFileLocation),
-                '01-01-2024',
-                10,
-                10,
-                hash_file(convertedFileLocation)
-            )
-        insert_gps_file(gpsFile)
-
+        gpsfile_in = parseFitFile(file_name)
+        # show name
+        print('file name: ' + gpsfile_in.name)
+        # insert into db
+        insert_gps_file(gpsfile_in, module_variables.TESTDBNAME)
--- run.py
+++ run.py
@@ -1,18 +1,14 @@
 from gpsconverter.ConvertGpsFile import parseFitFile
-from db_connection.db_methods import insert_gps_file, get_gps_file_by_name
+from db_connection.db_methods import (
+    insert_gps_file, 
+    get_gps_file_by_name, 
+    print_all_files
+)
+from db_connection.db_setup import clear_db, setup_db
+from db_feeder import insert_files_into_db
+import module_variables
 
-
-# read file
-data_folder = 'data/'
-file_name = 'Evening_Ride.fit'
-gpsfile_in = parseFitFile(data_folder, file_name)
-# show name
-print('file name: ' + gpsfile_in.name)
-# insert into db
-insert_gps_file(gpsfile_in)
-# get file from db
-gps_file_out = get_gps_file_by_name(file_name)
-# show name
-print(gps_file_out.name)
-
-
+#clear_db(module_variables.DBNAME)
+#setup_db(module_variables.DBNAME)
+#print_all_files(module_variables.DBNAME)
+insert_files_into_db()
