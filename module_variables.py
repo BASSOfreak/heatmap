79f242698beda1266ed0f8ae0c163f3012fe698b
BASSOfreak <bassofreak@gmail.com>
2025-01-17 21:32:21 +0100

    rename database files to work with sqlite plugin in VSC

--- module_variables.py
+++ module_variables.py
@@ -1,6 +1,6 @@
 BUF_SIZE = 65536
-INPUT_FILES_LOCATION = 'data/'
+INPUT_FILES_LOCATION = 'data/real_data'
 TEMP_OUTPUT_FILES_LOCATION = 'temp_output/'
 DEBUG_MODE = False
-DBNAME = 'db'
-TESTDBNAME = 'test/test_data/test_db'
+DBNAME = 'db.db'
+TESTDBNAME = 'test/test_data/test_db.db'
